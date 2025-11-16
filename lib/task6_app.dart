e707462f7a64b8a3e00040ec3e6fa69b716d09c5
Hoda Elkayal <helkayal169@gmail.com>
2025-11-16 20:40:57 +0200

    create custom button and app list screen

--- lib/task6_app.dart
+++ lib/task6_app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:task6_app/screens/business_card/business_card_screen.dart';
+import 'package:task6_app/screens/easylearn_screen/easylearn_screen.dart';
+import 'package:task6_app/screens/news/news_screen.dart';
+import 'package:task6_app/widgets/custom_button.dart';
 
 class Task6App extends StatelessWidget {
   const Task6App({super.key});
@@ -7,7 +11,34 @@ class Task6App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Task 6 App'))),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Task 6',
+            style: TextStyle(fontSize: 30, fontFamily: 'Pacifico'),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            spacing: 40,
+            children: [
+              Spacer(),
+              CustomButton(
+                title: 'Easy Learn Academy',
+                screen: EasylearnScreen(),
+              ),
+              CustomButton(
+                title: 'Business Card',
+                screen: BusinessCardScreen(),
+              ),
+              CustomButton(title: 'Flutter News', screen: NewsScreen()),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  final String title;
+  final Widget screen;
+  const CustomButton({super.key, required this.title, required this.screen});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return screen;
+            },
+          ),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(fontSize: 20, fontFamily: 'KodeMono'),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        fixedSize: const Size(300, 60),
+      ),
+      child: Text(title),
+    );
   }
 }
