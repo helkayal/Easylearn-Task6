bb3f3ca391ebc1c0b89e586c363bcfeab3a061a8
Hoda Elkayal <helkayal169@gmail.com>
2025-11-16 13:30:19 +0200

    add project empty files

---
+++ lib/screens/easylearn_screen/easylearn_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EasylearnScreen extends StatelessWidget {
+  const EasylearnScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('EasyLearn Screen')));
+  }
+}
---
+++ lib/task6_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Task6App extends StatelessWidget {
+  const Task6App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Task 6 App'))),
+    );
+  }
+}
